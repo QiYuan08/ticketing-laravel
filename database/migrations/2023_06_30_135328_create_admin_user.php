e104b4c829ad2ad8806b38fbc60bd6a612baa079
QiYuan08 <qteh0001@student.monash.edu>
2023-07-01 15:02:55 +0800

    fix migration file

--- database/migrations/2023_06_30_135328_create_admin_user.php
+++ database/migrations/2023_06_30_135328_create_admin_user.php
@@ -4,6 +4,7 @@ use App\Constant\Role;
 use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -15,15 +16,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('Users', function (Blueprint $table) {
-            User::create([
-                'id' => Role::ADMING_ACC_ID,
-                'name' => 'Admin',
-                'email' => 'admin@gmail.com',
-                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
-                'role_id' => 1
-            ]);
-        });
+        DB::table('users')->insert([
+            'id' => Role::ADMING_ACC_ID,
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            'role_id' => 1
+        ]);
+        
     }
 
     /**
